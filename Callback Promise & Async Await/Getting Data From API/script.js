db9a323467a37e44c83cee214072cc3048ad8156
Mahi <girimahesh.m@gmail.com>
2024-01-20 15:16:49 +0530

    User Card

---
+++ Callback Promise & Async Await/Getting Data From API/script.js
@@ -0,0 +1,31 @@
+const card_container = document.querySelector('.card-container');
+
+function getCard(id) {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', `https://dummyjson.com/user/${id}`);
+
+    request.send();
+
+    request.addEventListener('load', () => {
+        const jsonData = JSON.parse(request.responseText);
+
+        const card = `<div class="user-card">
+                    <img src=${jsonData.image} alt="Profile Image" />
+                    <h3 id="fname">${jsonData.firstName}</h3>
+                    <h3 id="lname">${jsonData.lastName}</h3>
+                    <p class="email">${jsonData.email}</p>
+                    <button class="btn">View Profile</button>
+                </div>`
+
+        card_container.insertAdjacentHTML('beforeend',card);
+    });
+}
+
+getCard(1);
+getCard(2);
+getCard(3);
+getCard(4);
+getCard(5);
+
+    
